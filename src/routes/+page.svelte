cd0af64d0470bb03c73ece1b256459dbe6926dee
Davy Kamanzi <davykamanzi@gmail.com>
2024-07-24 09:55:16 +0300

    Initialising landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,17 +2,17 @@
 	import "../app.pcss";
 </script>
 
-<div class="hero bg-base-200 min-h-screen">
+<main class="hero bg-base-200 min-h-screen">
 	<div class="hero-content text-center">
 		<div class="max-w-md">
 			<h1 class="text-5xl font-bold text-base-100">
 				un<span class="text-[#922529]">blog</span><span class="text-[#008C51]">able</span>.
 			</h1>
-			<p class="text-lg py-6">
+			<p class="hero-caption">
 				Isolate influencers sanitising the bloodthirsty Ruto regime on X (formerly Twitter) by
 				playing politics with the lives of innocent Kenyans!
 			</p>
-			<button class="btn btn-primary uppercase">Fanya ile kitu</button>
+			<button class="btn btn-primary uppercase text-lg">Fanya ile kitu</button>
 		</div>
 	</div>
-</div>
+</main>
