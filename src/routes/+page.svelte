b797856e20269128d856c638c4796c981d47e501
Davy Kamanzi <davykamanzi@gmail.com>
2024-07-24 09:51:10 +0300

    Initialising landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import "../app.pcss";
+</script>
+
+<div class="hero bg-base-200 min-h-screen">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold text-base-100">
+				un<span class="text-[#922529]">blog</span><span class="text-[#008C51]">able</span>.
+			</h1>
+			<p class="text-lg py-6">
+				Isolate influencers sanitising the bloodthirsty Ruto regime on X (formerly Twitter) by
+				playing politics with the lives of innocent Kenyans!
+			</p>
+			<button class="btn btn-primary uppercase">Fanya ile kitu</button>
+		</div>
+	</div>
+</div>
