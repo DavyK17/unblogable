2308eb0f1780037c6483c2acb3cf94b0afbfd2ac
Davy Kamanzi <davykamanzi@gmail.com>
2024-07-24 07:58:28 +0300

    Minor formatting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
 
 export default {
-    content: ["./src/**/*.{html,js,svelte,ts}"],
+	content: ["./src/**/*.{html,js,svelte,ts}"],
 
-    theme: {
-        extend: {}
-    },
+	theme: {
+		extend: {}
+	},
 
-    plugins: [require("@tailwindcss/typography")]
+	plugins: [typography]
 } as Config;
