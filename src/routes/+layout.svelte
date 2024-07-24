f77b0dfde1365dca587737ab5dea5925efe9c4be
Davy Kamanzi <davykamanzi@gmail.com>
2024-07-24 11:57:41 +0300

    Adding meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,12 @@
 	import "../app.pcss";
 </script>
 
+<svelte:head>
+	<title>#RutoMustGo | unblogable.</title>
+	<meta
+		name="description"
+		content="Boycott influencers sanitising the bloodthirsty Ruto regime on X (formerly Twitter) by playing politics with the lives of innocent Kenyans!"
+	/>
+</svelte:head>
+
 <slot />
