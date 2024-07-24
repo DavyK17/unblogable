668fa7ceb1cb48d8a5fa5b315b5e8a429ccd4036
Davy Kamanzi <davykamanzi@gmail.com>
2024-07-25 02:23:54 +0300

    Initialising schema

---
+++ supabase/migrations/20240724232222_unblogable.sql
@@ -0,0 +1,112 @@
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+ALTER DATABASE postgres SET datestyle = dmy;
+
+ALTER DATABASE postgres SET timezone = 'Africa/Nairobi';
+
+CREATE EXTENSION IF NOT EXISTS "pg_net" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "pgsodium" WITH SCHEMA "pgsodium";
+
+COMMENT ON SCHEMA "public" IS 'standard public schema';
+
+CREATE EXTENSION IF NOT EXISTS "pg_graphql" WITH SCHEMA "graphql";
+
+CREATE EXTENSION IF NOT EXISTS "pg_stat_statements" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "pgjwt" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "supabase_vault" WITH SCHEMA "vault";
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
+
+SET default_tablespace = '';
+
+SET default_table_access_method = "heap";
+
+CREATE TABLE IF NOT EXISTS "public"."blacklist" (
+    "x_user_id" "text" NOT NULL,
+    "x_handle" "text" NOT NULL,
+    "added_at" timestamp with time zone DEFAULT "now"() NOT NULL,
+    CONSTRAINT "blacklist_x_handle_check" CHECK (("x_handle" ~ '^\d+$'::"text"))
+);
+
+ALTER TABLE "public"."blacklist" OWNER TO "postgres";
+
+CREATE TABLE IF NOT EXISTS "public"."boycotted_influencers" (
+    "user_id" "uuid" NOT NULL,
+    "influencer_id" "text" NOT NULL,
+    "boycotted_at" timestamp with time zone DEFAULT "now"() NOT NULL
+);
+
+ALTER TABLE "public"."boycotted_influencers" OWNER TO "postgres";
+
+ALTER TABLE ONLY "public"."blacklist"
+    ADD CONSTRAINT "blacklist_pkey" PRIMARY KEY ("x_user_id");
+
+ALTER TABLE ONLY "public"."blacklist"
+    ADD CONSTRAINT "blacklist_x_handle_key" UNIQUE ("x_handle");
+
+ALTER TABLE ONLY "public"."boycotted_influencers"
+    ADD CONSTRAINT "boycotted_influencers_pkey" PRIMARY KEY ("user_id", "influencer_id");
+
+ALTER TABLE ONLY "public"."boycotted_influencers"
+    ADD CONSTRAINT "public_boycotted_influencers_influencer_id_fkey" FOREIGN KEY ("influencer_id") REFERENCES "public"."blacklist"("x_user_id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY "public"."boycotted_influencers"
+    ADD CONSTRAINT "public_boycotted_influencers_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth"."users"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+CREATE POLICY "Enable read access for all users" ON "public"."blacklist" FOR SELECT USING (true);
+
+ALTER TABLE "public"."blacklist" ENABLE ROW LEVEL SECURITY;
+
+ALTER TABLE "public"."boycotted_influencers" ENABLE ROW LEVEL SECURITY;
+
+ALTER PUBLICATION "supabase_realtime" OWNER TO "postgres";
+
+GRANT USAGE ON SCHEMA "public" TO "postgres";
+GRANT USAGE ON SCHEMA "public" TO "anon";
+GRANT USAGE ON SCHEMA "public" TO "authenticated";
+GRANT USAGE ON SCHEMA "public" TO "service_role";
+
+GRANT ALL ON TABLE "public"."blacklist" TO "anon";
+GRANT ALL ON TABLE "public"."blacklist" TO "authenticated";
+GRANT ALL ON TABLE "public"."blacklist" TO "service_role";
+
+GRANT ALL ON TABLE "public"."boycotted_influencers" TO "anon";
+GRANT ALL ON TABLE "public"."boycotted_influencers" TO "authenticated";
+GRANT ALL ON TABLE "public"."boycotted_influencers" TO "service_role";
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "service_role";
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "service_role";
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "service_role";
+
+RESET ALL;
+
+--
+-- Dumped schema changes for auth and storage
+--
+
