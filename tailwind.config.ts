c9c9821df371b330ae407ff5994b778c868490e2
Davy Kamanzi <davykamanzi@gmail.com>
2024-07-24 09:50:59 +0300

    Adjusting themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
+
 import typography from "@tailwindcss/typography";
 import daisyui from "daisyui";
+import { black, lofi } from "daisyui/src/theming/themes";
 
 export default {
 	content: ["./src/**/*.{html,js,svelte,ts}"],
@@ -15,27 +17,14 @@ export default {
 		themes: [
 			{
 				light: {
-					primary: "#03a1fb",
-					secondary: "#166b9c",
-					accent: "#012032",
-					neutral: "#011118",
-					"base-100": "#eff3f5",
-					info: "#654401",
-					success: "#02ca4b",
-					warning: "#bd490f",
-					error: "#ca0241"
-				},
-
+					...lofi,
+					"base-100": "#000"
+				}
+			},
+			{
 				dark: {
-					primary: "#03a1fb",
-					secondary: "#63b8e9",
-					accent: "#cdecfe",
-					neutral: "#e6f6fe",
-					"base-100": "#011118",
-					info: "#fed481",
-					success: "#0fbd4f",
-					warning: "#f07c42",
-					error: "#fd1c63"
+					...black,
+					"base-100": "#fff"
 				}
 			}
 		]
