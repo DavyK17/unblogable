83659ca4e02f5034cf6cc70b85ec01cc12c5b579
Davy Kamanzi <davykamanzi@gmail.com>
2024-07-24 23:53:16 +0300

    Fleshing out landing page + adding dummy blacklist

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,76 @@
 <main class="hero min-h-screen">
 	<div class="hero-content text-center">
 		<div class="max-w-md">
-			<h1 class="text-5xl font-bold text-base-100">
+			<h1 class="text-5xl font-bold font-serif">
 				un<span class="text-red">blog</span><span class="text-green">able</span>.
 			</h1>
 			<p class="hero-caption">
 				Boycott influencers sanitising the bloodthirsty Ruto regime on X (formerly Twitter) by
-				playing politics with the lives of innocent Kenyans!
+				spreading misinformation and playing politics with the lives of innocent Kenyans!
 			</p>
-			<button class="btn btn-primary uppercase text-lg font-semibold">Fanya ile kitu</button>
+			<button class="hero-button">Connect your account</button>
 		</div>
 	</div>
 </main>
+<section class="w-full p-4">
+	<div class="container mx-auto max-w-3xl">
+		<div class="prose blacklist m-0 max-w-full">
+			<h2 class="prose-headings:h2">Blacklist</h2>
+			<ul>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+				<li>Abel</li>
+			</ul>
+		</div>
+	</div>
+</section>
+<footer class="w-full p-8 mt-20">
+	<div class="container mx-auto">
+		<p class="text-4xl text-center text-red">#RutoMustGo</p>
+	</div>
+</footer>
