80ab62afcee2040b381d19341735f9c11a24b75c
Davy Kamanzi <davykamanzi@gmail.com>
2024-07-25 04:55:41 +0300

    Removing Supabase client export

--- src/lib/supabase/index.ts
+++ src/lib/supabase/index.ts
@@ -1,6 +1,4 @@
 import type { Database as DatabaseGenerated } from "./database-generated";
 import type { Database } from "./database";
-import supabase from "./client";
 
 export type { Database, DatabaseGenerated };
-export { supabase };
